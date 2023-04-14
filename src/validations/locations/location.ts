5ff6dc8a8c7f9ccdccc76b375d18c7958bda605e
ajay2966 <ajmadhpuriya1234@gmail.com>
2023-04-14 19:01:39 +0530

    remove location alpha numeric validation

--- src/validations/locations/location.ts
+++ src/validations/locations/location.ts
@@ -2,6 +2,6 @@ import * as Yup from "yup";
 
 export const location = Yup.object().shape({
   location: Yup.string()
-    .matches(/^[.a-zA-Z0-9,!? ]*$/, "Allowed Characters: Alpha Numeric")
+    // .matches(/^[.a-zA-Z0-9,!? ]*$/, "Allowed Characters: Alpha Numeric")
     .required("field is required"),
 });
