c04c500a58195eba0917d8cb9839a4f9fffa1b00
ajay2966 <ajmadhpuriya1234@gmail.com>
2023-04-20 20:04:03 +0530

    change grid validation

--- src/validations/grid/grid.ts
+++ src/validations/grid/grid.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const grid = Yup.object().shape({
   gridId: Yup.string()
-    .matches(/^[.a-zA-Z0-9,!? ]*$/, "Allowed Characters: Alpha Numeric")
+    .matches(/^G[0-9]{3}-[0-9]{2}$/g, "Enter a valid virtual id.")
     .required("field is required"),
   location: Yup.string().required("field is required"),
 });
