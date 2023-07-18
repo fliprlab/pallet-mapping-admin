3f08087be448df0dab6d5c99915f089745acd559
Abhay jatav <abhayjatav777@gmail.com>
2023-07-18 16:58:35 +0530

    grid ctive inactive work

--- src/hooks/api-urls/grid.api.ts
+++ src/hooks/api-urls/grid.api.ts
@@ -3,4 +3,5 @@ import { adminApiPrefix } from "../../constants";
 export const gridApis = {
   GET_GRIDS: adminApiPrefix + "get-grids",
   CREATE_GRID: adminApiPrefix + "create-grid",
+  INACTIVE_GRID: adminApiPrefix + "inactive-grid",
 };
---
+++ src/pages/grid-list/components/ActionBtn.tsx
@@ -0,0 +1,42 @@
+import { ActionIcon, Box, Tooltip } from "@mantine/core";
+import { IconEye, IconEyeOff } from "@tabler/icons";
+import React, { memo } from "react";
+import { useInactiveGridMutation } from "../../../hooks/grid/mutation/useInactiveGrid.mutation";
+import { showNotification } from "@mantine/notifications";
+
+interface IActionBtn {
+  data: { _id: string; active: boolean };
+}
+
+const ActionBtn: React.FC<IActionBtn> = ({ data: { active = true, _id } }) => {
+  const { mutateAsync, isLoading } = useInactiveGridMutation({
+    itemPerPage: 50,
+    page: 1,
+    inactive: active ? "active" : "in-active",
+  });
+
+  const handleInactive = async () => {
+    const res = await mutateAsync({ _id, active });
+    if (res.status === "success") {
+      showNotification({ message: res.message, color: "green" });
+    } else {
+      showNotification({ message: res.data.message, color: "red" });
+    }
+  };
+
+  return (
+    <Box>
+      <Tooltip label={active ? `In-Active Grid` : `Active Grid`}>
+        <ActionIcon
+          disabled={isLoading}
+          loading={isLoading}
+          onClick={handleInactive}
+        >
+          {active ? <IconEye /> : <IconEyeOff />}
+        </ActionIcon>
+      </Tooltip>
+    </Box>
+  );
+};
+
+export default memo(ActionBtn);
