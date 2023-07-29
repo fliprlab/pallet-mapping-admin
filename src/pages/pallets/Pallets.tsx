277b224574f333d97048efda15d437bc2b95df5d
Abhay jatav <abhayjatav777@gmail.com>
2023-07-29 19:10:27 +0530

    Task-1221-Admin-Location-Search-on-Pallet

--- src/pages/pallets/Pallets.tsx
+++ src/pages/pallets/Pallets.tsx
@@ -1,38 +1,29 @@
-import { Box, Text } from "@mantine/core";
+import { Box, Flex, Text } from "@mantine/core";
 import React, { memo, useMemo, useState } from "react";
 import CustomTableWithHeader from "../../components/table/CustomTableWithHeader";
 import { COLORS } from "../../colors";
 import { useGetPallets } from "../../hooks/pallets/query/useGetPallets";
 import { TABLE_PAGE_LIMIT } from "../../constants";
-import { showNotification } from "@mantine/notifications";
+
 import { COLUMNS } from "../../columns";
+import StatusMenu from "../../components/status-menu/StatusMenu";
 
 function Pallets() {
   const [pagedData, setPagedData] = useState({ total: 0 });
   const [activePage, setActivePage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
 
-  const { isLoading, data } = useGetPallets(
-    {
-      itemPerPage: TABLE_PAGE_LIMIT,
-      page: activePage,
-    },
-    {
-      onSuccess: (res) => {
-        if (res.status === "success") {
-          setPagedData(res.pageData ? res.pageData : { total: 0 });
-        } else {
-          showNotification({
-            message: res.data.message,
-            color: "red",
-          });
-        }
-      },
-      enabled: true,
-    }
-  );
+  const { isLoading, data } = useGetPallets({
+    itemPerPage: TABLE_PAGE_LIMIT,
+    page: activePage,
+    search,
+    status,
+  });
 
-  const locations = useMemo(() => {
+  const pallets = useMemo(() => {
     if (!isLoading && data) {
+      setPagedData(data.pageData ? data.pageData : { total: 0 });
       return data.data;
     } else {
       return [];
@@ -47,9 +38,23 @@ function Pallets() {
         </Text>
       </Box>
       <CustomTableWithHeader
+        rightComponent={
+          <Flex>
+            <StatusMenu
+              status={status}
+              options={[
+                { label: "Assign Grid", value: "pallet-asign-grid" },
+                { label: "Created", value: "pallet-created" },
+                { label: "Out", value: "pallet-out" },
+              ]}
+              setStatus={setStatus}
+            />
+          </Flex>
+        }
+        onChangeSearch={setSearch}
         isLoading={isLoading}
-        search={false}
-        data={locations}
+        search={true}
+        data={pallets}
         columns={COLUMNS.palletsColumns}
         paginationProps={{ activePage, pagedData, setActivePage }}
       />
